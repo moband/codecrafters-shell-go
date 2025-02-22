8428e22473cac4dfcc48209db146d7bc89cb437b
MohammadReza Alagheband <alagheband@live.com>
2025-02-22 14:07:08 +0100

    [any message]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -10,15 +10,16 @@ import (
 var _ = fmt.Fprint
 
 func main() {
-	// Uncomment this block to pass the first stage
-	fmt.Fprint(os.Stdout, "$ ")
+	for {
+		// Uncomment this block to pass the first stage
+		fmt.Fprint(os.Stdout, "$ ")
 
-	// Wait for user input
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		fmt.Fprint(os.Stderr, err)
-		os.Exit(1)
+		// Wait for user input
+		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			fmt.Fprint(os.Stderr, err)
+			os.Exit(1)
+		}
+		fmt.Println(command[:len(command)-1] + ": command not found")
 	}
-	fmt.Println(command[:len(command)-1] + ": command not found")
-
 }
